69a4908b39bf55dc83d84c7ce44355b306189a6e
rmenai <rami.menai@outlook.com>
2022-05-15 06:07:04 +0300

    Remove intlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,7 @@ import { defineNuxtConfig } from "nuxt";
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   srcDir: "src/",
-  buildModules: [
-    "@pinia/nuxt",
-    "@vueuse/nuxt",
-    "@intlify/nuxt3",
-    "nuxt-windicss",
-  ],
+  buildModules: ["@pinia/nuxt", "@vueuse/nuxt", "nuxt-windicss"],
   modules: ["@nuxtjs/color-mode"],
   build: { transpile: ["three"] },
   vite: {
@@ -20,13 +15,4 @@ export default defineNuxtConfig({
   css: ["assets/css/base.scss"],
   // @ts-ignore
   colorMode: { classSuffix: "" },
-  intlify: {
-    localeDir: "locales",
-    vueI18n: {
-      legacy: false,
-      locale: "en",
-      fallbackLocale: "en",
-      globalInjection: true,
-    },
-  },
 });
